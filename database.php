958df982d2c303e297783d97a3e56f2562bee145
alvancahya <62916602+alvancahya@users.noreply.github.com>
2020-03-31 07:46:10 +0700

    Create database.php

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbusername = 'root';
+$dbpassword = 'ubaya';
+$dbname = 'sampledb';
+
+$c = new mysqli($dbhost,$dbusername,$dbpassword,$dbname);
+if($c->connect_error){
+echo "unable to connect, please try again";
+die();
+}
+?>
